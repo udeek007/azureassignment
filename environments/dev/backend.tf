3e88a86b8896344bac1f4d6461fdb68fe2e0ff54
udeek007 <udeek077@gmail.com>
2025-04-16 23:48:38 +0530

    clean code

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,4 +1,8 @@
-resource_group_name  = "dev-governance-rg"
-storage_account_name = "devtfstateudk"
-container_name       = "dev-tfstate-container"
-key                  = "dev.terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "dev-governance-rg"
+    storage_account_name = "devtfstateudk"
+    container_name       = "dev-tfstate-container"
+    key                  = "dev.terraform.tfstate"
+  }
+}
